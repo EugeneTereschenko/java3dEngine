de45d1403a29324676b89862a059fc2e6da71075
Yevhen_Tereshchenko <Yevhen_Tereshchenko@epam.com>
2025-02-02 17:33:27 +0200

    add files

--- src/main/java/com/example/graphics/Render3D.java
+++ src/main/java/com/example/graphics/Render3D.java
@@ -1,19 +1,34 @@
 package com.example.graphics;
 
 public class Render3D extends Render {
+
     public Render3D(int width, int height) {
         super(width, height);
     }
 
+    double time = 0;
+
     public void floor() {
         for (int y = 0; y < height; y++){
-            double yDepth = y - height / 2.4;
-            double z  = 100.0 / yDepth;
+            double celling = (y - height / 2.0) / height;
+
+
+            if (celling  < 0) {
+                celling = -celling;
+            }
+
+            double z  = 8 / celling;
+
+            time += 0.0005;
+
             for (int x = 0; x < width; x++){
-                double depth =  x - width / 2;
+                double depth =  (x - width / 2.0) / height;
                 depth *= z;
-                int xx = (int) (depth) & 5;
-                pixels[x + y * width] = xx * 128;
+                double xx = depth;
+                double yy = z + time;
+                int xPix = (int) (xx);
+                int yPix = (int) (yy);
+                pixels[x + y * width] = ((xPix & 15) * 16) | ((yPix & 15) * 16) << 8;
             }
         }
     }
